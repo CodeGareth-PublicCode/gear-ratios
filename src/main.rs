332d3f429044ef9db46eef5cd911aef124f66668
Gareth Mahoney <gmahoney1992@gmail.com>
2023-12-06 15:12:22 +0000

    [update] add scratch logic for mapping TT combos to coordinates

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn logic_docs(){
+fn logic_docs() {
     /// Dot inverse structure
     /// a1 a2
     /// b1 b2
@@ -41,20 +41,17 @@ fn logic_docs(){
     println!("brain-dump")
 }
 
-
-
 #[cfg(test)]
 mod tests {
-    use itertools::{interleave, peek_nth, Itertools, izip};
+    use itertools::{interleave, izip, peek_nth, Itertools};
     use ndarray::iter::Windows;
-    use ndarray::{Array, Ix};
     use ndarray::ArrayView;
     use ndarray::Dim;
+    use ndarray::{Array, Ix};
     use std::collections::HashMap;
 
     #[test]
     fn test_masking_logic_to_find_all_that_are_not_a_dot() {
-
         let giant: Vec<&str> = vec![
             "4", "6", "7", ".", ".", "1", "1", "4", ".", ".", ".", ".", ".", "*", ".", ".", ".",
             ".", ".", ".", ".", ".", "3", "5", ".", ".", "6", "3", "3", ".",
@@ -92,7 +89,7 @@ mod tests {
 
         let not_a_dot_mask: Vec<Array<bool, Dim<[Ix; 2]>>> = windows
             .iter()
-            .map(|window| window.map(|element| element.to_owned() != ".") )
+            .map(|window| window.map(|element| element.to_owned() != "."))
             .collect();
 
         // Test you can parse a window as expected
@@ -105,17 +102,37 @@ mod tests {
         // [[7,.] <---- should be [T,F] referenced as a1, a2
         //  [.,*]] <---- should be [F,T] referenced as b1, b2
 
-        let a1: bool = not_a_dot_mask.get(2).unwrap().get((0,0)).unwrap().to_owned();
-        let a2: bool = not_a_dot_mask.get(2).unwrap().get((0,1)).unwrap().to_owned();
-        let b1: bool = not_a_dot_mask.get(2).unwrap().get((1,0)).unwrap().to_owned();
-        let b2: bool = not_a_dot_mask.get(2).unwrap().get((1,1)).unwrap().to_owned();
+        let a1: bool = not_a_dot_mask
+            .get(2)
+            .unwrap()
+            .get((0, 0))
+            .unwrap()
+            .to_owned();
+        let a2: bool = not_a_dot_mask
+            .get(2)
+            .unwrap()
+            .get((0, 1))
+            .unwrap()
+            .to_owned();
+        let b1: bool = not_a_dot_mask
+            .get(2)
+            .unwrap()
+            .get((1, 0))
+            .unwrap()
+            .to_owned();
+        let b2: bool = not_a_dot_mask
+            .get(2)
+            .unwrap()
+            .get((1, 1))
+            .unwrap()
+            .to_owned();
 
         assert_eq!(a1, true);
         assert_eq!(a2, false);
         assert_eq!(b1, false);
         assert_eq!(b2, true);
 
-        assert_eq!((a1,b2), (true, true)); // this is the target column
+        assert_eq!((a1, b2), (true, true)); // this is the target column
 
         // chunked to enable enumeration to reflect row anchor
         // In a 2x2 window crawl, you shouldn't expect to see the last column as the window
@@ -124,12 +141,10 @@ mod tests {
         let chunked_masks = not_a_dot_mask.chunks(chunk_length);
 
         assert_eq!(chunked_masks.len(), 3)
-
     }
 
     #[test]
-    fn test_chunking_windows_to_be_able_to_trace_mask_array_back_to_root_array(){
-
+    fn test_chunking_windows_to_be_able_to_trace_mask_array_back_to_root_array() {
         let giant: Vec<&str> = vec![
             "4", "6", "7", ".", ".", "1", "1", "4", ".", ".", ".", ".", ".", "*", ".", ".", ".",
             ".", ".", ".", ".", ".", "3", "5", ".", ".", "6", "3", "3", ".",
@@ -176,7 +191,7 @@ mod tests {
 
         let not_a_dot_mask: Vec<Array<bool, Dim<[Ix; 2]>>> = windows
             .iter()
-            .map(|window| window.map(|element| element.to_owned() != ".") )
+            .map(|window| window.map(|element| !(element.to_owned() == ".")))
             .collect();
 
         let chunk_length = binding.row(0).len() - 1;
@@ -193,24 +208,48 @@ mod tests {
             window_store.entry(anchor_row).or_insert(chunk);
         }
 
-        // test against an expected window (used in previous tests)
-
         let anchor_row_zero = window_store.get(&0).unwrap();
         let target_window = anchor_row_zero.get(2).unwrap();
 
-        let a1: bool = target_window.get((0,0)).unwrap().to_owned();
-        let a2: bool = target_window.get((0,1)).unwrap().to_owned();
-        let b1: bool = target_window.get((1,0)).unwrap().to_owned();
-        let b2: bool = target_window.get((1,1)).unwrap().to_owned();
+        let a1: bool = target_window.get((0, 0)).unwrap().to_owned();
+        let a2: bool = target_window.get((0, 1)).unwrap().to_owned();
+        let b1: bool = target_window.get((1, 0)).unwrap().to_owned();
+        let b2: bool = target_window.get((1, 1)).unwrap().to_owned();
 
-        assert_eq!(a1, true);
-        assert_eq!(a2, false);
-        assert_eq!(b1, false);
-        assert_eq!(b2, true);
+        let combinations: [(bool, bool); 6] =
+            [(a1, a2), (b1, b2), (a1, b1), (a2, b2), (b1, a2), (a1, b2)];
 
-        assert_eq!((a1,b2), (true, true)); // this is the target column
+        for (position, &combo) in combinations.iter().enumerate() {
+            dbg!(position);
 
-        // todo make use of enumerate to translate back to main grid
+            if combo == (true, true) {
+                let window_coordinates_for_known_symbol_and_adjacent_number = (map_position_to_coordinate(position));
+                dbg!(target_window);
+                assert_eq!(([0,0],[1,1]),window_coordinates_for_known_symbol_and_adjacent_number)
+            }
+        }
 
+        // to do - is it possible to then establish your row number then relative position to main grid?
+
+    }
+
+    fn map_position_to_coordinate(position: usize) -> ([i32; 2], [i32; 2]) {
+
+        // [(a1, a2),
+        //  (b1, b2),
+        //  (a1, b1),
+        //  (a2, b2),
+        //  (b1, a2),
+        //  (a1, b2)];
+
+        match position {
+            0 => ([0,0],[0,1]),
+            1 => ([1,0],[1,1]),
+            2 => ([0,0],[1,0]),
+            3 => ([0,1],[1,1]),
+            4 => ([1,0],[0,1]),
+            5 => ([0,0],[1,1]),
+            _ => ([9,9],[9,9]),
+        }
     }
 }
