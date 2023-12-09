25f993744c2d125a7b42a33b1e9a17ea1ac4511b
Gareth Mahoney <gmahoney1992@gmail.com>
2023-12-09 22:12:07 +0000

    [update] clean refactor with more obvious data usage

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,97 @@
-use ndarray::iter::Windows;
-use ndarray::{array, Array, ArrayBase, ArrayView, Dim, Ix, OwnedRepr};
 use std::slice::Chunks;
+use ndarray::{Array, ArrayBase, ArrayView, Dim, Ix, OwnedRepr};
 
 fn main() {
     println!("Hello");
 }
 
-pub fn parse_string_to_array<'a>(input: &str) -> Vec<&str> {
-    let array = input.split("").filter(|&s| s != "").collect();
-    array
+pub fn parse_string_to_array(input: &str) -> Vec<String> {
+    input.chars().into_iter().map(|char| String::from(char)).collect()
+}
+pub fn form_multidimensional_array(input: &Vec<String>) -> Array<String, Dim<[Ix; 2]>> {
+    Array::from_shape_vec((2,7), input.to_vec()).unwrap()
 }
 
-pub fn form_multidimensional_array(
-    array_dimensions: Dim<[usize; 2]>,
-    raw_input: Vec<&str>,
-) -> ArrayBase<OwnedRepr<&str>, Dim<[usize; 2]>> {
-    Array::from_shape_vec(array_dimensions, raw_input).unwrap()
+pub fn form_window_array(input: &ArrayBase<OwnedRepr<String>, Dim<[Ix; 2]>>) -> Vec<ArrayView<String, Dim<[Ix; 2]>>> {
+    let windows: Vec<ArrayView<String, Dim<[Ix; 2]>>> = input.windows(Dim((2, 2))).into_iter().collect();
+    windows
 }
 
-pub fn form_two_by_two_window_views_of_multidimensional_array<'b>(
-    raw_input: &[&'b str], array_dimensions: Dim<[usize; 2]>,
+pub fn form_boolean_mask(input: &Vec<ArrayView<String, Dim<[Ix; 2]>>>) -> Vec<Array<bool, Dim<[Ix; 2]>>> {
+    input.iter().map(|window| window.map(|element| !(element.to_owned() == "."))).collect()
+}
 
-) -> Windows<'b, &'b str, Dim<[usize; 2]>> {
+pub fn chunk_window_array(input: &Vec<Array<bool, Dim<[Ix; 2]>>>, chunk_size: usize) -> Chunks<Array<bool, Dim<[Ix; 2]>>> {
 
-    Array::from_shape_vec(array_dimensions, raw_input.to_vec()).unwrap().windows(Dim((2, 2)))
+    input.chunks(chunk_size)
 
 }
 
-pub fn form_boolean_mask_based_on_dot_or_not<'a>(
-    input_array: Vec<ArrayView<'a, &'a str, Dim<[Ix; 2]>>>,
-) -> Vec<Array<bool, Dim<[Ix; 2]>>> {
-    input_array
-        .iter()
-        .map(|window| window.map(|element| !(element.to_owned() == ".")))
-        .collect()
-}
-
-pub fn chunk_boolean_arrays_to_reflect_rows(
-    boolean_arrays: &Vec<Array<bool, Dim<[Ix; 2]>>>,
-    chunk_size: usize,
-) -> Chunks<Array<bool, Dim<[Ix; 2]>>> {
-    let iter = boolean_arrays.chunks(chunk_size);
-    iter
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use itertools::assert_equal;
-    use ndarray::{Array2, Ix2};
+    use ndarray::{Array2};
 
     #[test]
     fn test_parse_string_to_array() {
         let giant_string = "467..114.....*";
         let array = parse_string_to_array(giant_string);
-        assert_eq!(
-            array.get(13).unwrap(),
-            &"*"
-        );
+        let expected_array = vec!["4","6","7",".",".","1","1","4",".",".",".",".",".","*"];
+        assert_eq!(array, expected_array);
     }
 
     #[test]
-    fn test_form_multidimensional_array() {
+    fn test_multidimensional_array() {
         let giant_string = "467..114.....*";
         let array = parse_string_to_array(giant_string);
-        let multidimensional_array = form_multidimensional_array(Dim((2, 7)), array);
+        let expected_array = vec!["4","6","7",".",".","1","1","4",".",".",".",".",".","*"];
 
-        let expected_array = Array2::from_shape_vec(Dim((2,2)), vec![
-            "4", "6", "7", ".", ".", "1", "1",
-            "4", ".", ".", ".", ".", ".", "*"
-        ]).unwrap();
-
-        assert_eq!(multidimensional_array,expected_array);
+        let multi_array = form_multidimensional_array(&array);
+        let expected = Array2::from_shape_vec((7,2),expected_array).unwrap();
+        assert_eq!(multi_array, expected);
     }
 
     #[test]
-    fn test_form_window_array() {
+    fn test_window_array() {
         let giant_string = "467..114.....*";
         let array = parse_string_to_array(giant_string);
-        let multidimensional_array = form_multidimensional_array(Dim((2, 7)), array);
+        let multi_array = form_multidimensional_array(&array);
+        let window_array = form_window_array(&multi_array);
 
-        let array_for_boolean = parse_string_to_array(giant_string);
-        let windows =
-            form_two_by_two_window_views_of_multidimensional_array(&array_for_boolean, Dim((2, 7)));
+        let expected = Array2::from_shape_vec((2,2),vec!["4","6","7","."]).unwrap();
+        assert_eq!(expected, window_array.get(0).unwrap())
     }
+
+    #[test]
+    fn test_form_boolean_mask() {
+        let giant_string = "467..114.....*";
+        let array = parse_string_to_array(giant_string);
+        let multi_array = form_multidimensional_array(&array);
+        let window_array = form_window_array(&multi_array);
+
+        let boolean_mask = form_boolean_mask(&window_array);
+        let expected = Array2::from_shape_vec((2,2),vec![true,true,true,false]).unwrap();
+
+        assert_eq!(boolean_mask.get(0).unwrap(), expected);
+
+    }
+
+    #[test]
+    fn test_form_chunk_boolean_window_array() {
+        let giant_string = "467..114.....*";
+        let array = parse_string_to_array(giant_string);
+        let multi_array = form_multidimensional_array(&array);
+
+        println!("{}", &multi_array);
+
+        let window_array = form_window_array(&multi_array);
+        let boolean_mask = form_boolean_mask(&window_array);
+        let chunked_window_array = chunk_window_array(&boolean_mask, 6);
+
+        // Given a max row of 7, you will never iterate on to just the last column
+        // You will stop short in order to satisfy the 2 x 2 window
+        assert_eq!(chunked_window_array.len(), 1);
+    }
+
 }
